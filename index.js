0159ca02fa7f230c5371d7974417fa5ce106b95e
Oleg Chursin <olegchursin@gmail.com>
2017-11-07 21:35:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments){
+
+  var beatlesPlayArray = [];
+
+  for (var i = 0; i < musicians.length; i++){
+    beatlesPlayArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return beatlesPlayArray;
+}
